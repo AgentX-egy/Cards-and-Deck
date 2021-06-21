468aef37dd9e1b5f48a2b4f046e4316a9d593548
AbdulRahman Essam Sayed Heikal <starfire19990@gmail.com>
2021-06-21 04:19:44 +0200

    Redesign memory allocation scheme, no more memeory leak

--- Card Game/Card Game.cpp
+++ Card Game/Card Game.cpp
@@ -2,54 +2,11 @@
 //
 
 #include "Deck.h"
-Deck::Deck* makeDeck(const int& size) {
-	CardsQueue dummy = CardsQueue();
-	CardsQueue* deckTop = &dummy;
-	CardsQueue* deckBottom = deckTop;
-	for (int rcounter = 1; rcounter < size + 1; rcounter++)
-	{
-		CardsQueue* current = new CardsQueue(Cards((Ranks)rcounter, Suites::DECK));
-		deckBottom->append(current);
-		deckBottom = current;
-	}
-	deckTop = deckTop->nextCard;
-	return new Deck::Deck(deckTop, deckBottom);
-}
 
-#define RUN_TEST0 1
-#define RUN_TEST1 0
 int main()
 {
-#if RUN_TEST0
 	{
-		//CutAt(int) TestCases
-		int testCases[] = { -5, 0, 1, 4, 5, 800 };
-		printf("running TEST0\n...........");
-		for (int i = 0; i < 6; i++)
-		{
-			printf("\nTESTCASE-%d\n", i);
-			Deck::Deck* deck = makeDeck(5);
-			Deck::Deck* x = deck->cut(testCases[i]);
-			(deck->deckTop != nullptr) ? deck->print() : (void)printf("deck set to nullptr\n");
-			printf("----------\n");
-			(x->deckTop != nullptr) ? x->print() : (void)printf("deck set to nullptr\n");
-		}
+		Deck::Deck d = Deck::deck52();
+		std::cout << d.pickACard(51) <<std::endl; //choosing 52 will case error as it's the last cards, see ::pop();
 	}
-#endif
-#if RUN_TEST1
-	{
-		//CutAt(int,int) TestCases
-		int testCases[5][2] = { {2,3}, {0,2}, {1,4}, {0,4}, {3,3} };
-		printf("running TEST1");
-		for (int i = 0; i < 5; i++)
-		{
-			printf("\nTESTCASE-%d\n", i);
-			Deck::Deck* deck = makeDeck(5);
-			Deck::Deck* x = deck->cut(testCases[i][0], testCases[i][1]);
-			(deck->deckTop != nullptr) ? deck->print() : (void)printf("deck set to nullptr\n");
-			printf("----------\n");
-			(x->deckTop != nullptr) ? x->print() : (void)printf("deck set to nullptr\n");
-		}
-	}
-#endif
 }
